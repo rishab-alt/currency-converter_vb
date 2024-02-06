c8bce7f9ca22925abef3d33f4d6a9fd8a1803ae9
Rish Saddiq <1517120@stu.tmc.ac.uk>
2024-02-06 13:33:43 +0000

    Visual Basics file changes

--- currency converter/Form1.vb
+++ currency converter/Form1.vb
@@ -1,26 +1,27 @@
-﻿
-Imports System.Windows
-
-Public Class Form1
+﻿Public Class Form1
+    'items declared in combo box 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         from_Currency.Items.Add("USD")
         from_Currency.Items.Add("EUR")
         from_Currency.Items.Add("GBP")
+        from_Currency.Items.Add("AED")
 
         to_Currency.Items.Add("USD")
         to_Currency.Items.Add("EUR")
         to_Currency.Items.Add("GBP")
+        to_Currency.Items.Add("AED")
     End Sub
+    ' the conversion is calculated and output 
+    Private Sub calculate_Click(sender As Object, e As EventArgs) Handles calculate.Click
+        Dim conversionRate As Double = Convert.ToDouble(Conversion.Text)
 
-    Private Sub calcuate_Click(sender As Object, e As EventArgs) Handles calculate.Click
-        Dim conversionRate As Double = Convert.ToDouble(conversionRate)
-        Dim total As Double = Convert.ToDouble(amount.Text)
+        Dim totalAmount As Double = Convert.ToDouble(amount.Text)
 
-        Dim convertTotal As Double = conversionRate * total
+        Dim convertTotal As Double = conversionRate * totalAmount
 
-        result.Text = " From " & from_Currency.Text & " at " & conversionRate & " to " & to_Currency.Text & total & " Total is " & convertTotal
+        showResult.Text = " From " & from_Currency.Text & " at " & conversionRate & " to " & to_Currency.Text & " Total is " & convertTotal
     End Sub
-
+    'checks if the from and to currencies are the SAME
     Private Sub From_currency_SelectedIndexChanged(sender As Object, e As EventArgs) Handles from_Currency.SelectedIndexChanged
         If from_Currency.SelectedItem IsNot Nothing AndAlso to_Currency.SelectedItem IsNot Nothing Then
             If from_Currency.SelectedItem.ToString().Equals(to_Currency.SelectedItem.ToString()) Then
@@ -28,8 +29,6 @@ Public Class Form1
                 from_Currency.SelectedIndex = -1
             End If
         End If
-
-
     End Sub
 
     Private Sub to_Currency_SelectedIndexChanged(sender As Object, e As EventArgs) Handles to_Currency.SelectedIndexChanged
@@ -40,9 +39,10 @@ Public Class Form1
             End If
         End If
     End Sub
-
+    ' shows the help page 
     Private Sub help_Click(sender As Object, e As EventArgs) Handles help.Click
         Dim help As New help()
         help.Show()
     End Sub
+
 End Class
